540a5e009f2baa273ab9f0de5691c769191d5969
Oikos.cash <albert@oikos.cash>
2021-05-22 19:26:27 -0400

    fix logos for metamask

--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -3,4 +3,4 @@ import { currencyKeyToIconMap, CurrencyKey } from 'constants/currency';
 export const getCurrencyKeyIcon = (currencyKey: CurrencyKey) => currencyKeyToIconMap[currencyKey];
 
 export const getCurrencyKeyURLPath = (currencyKey: CurrencyKey) =>
-	`https:///www.synthetix.io/assets/synths/svg/${currencyKey}.svg`;
+	`https://minter.oikos.cash/images/currencies/${currencyKey}.svg`;

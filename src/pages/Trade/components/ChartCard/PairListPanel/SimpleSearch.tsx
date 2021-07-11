3b5a9976ba8a075f4407b1033cf62ef9e8db5976
Albert Rodriguez <59787133+oikos1@users.noreply.github.com>
2021-07-10 22:47:43 -0400

    fix typo

--- src/pages/Trade/components/ChartCard/PairListPanel/SimpleSearch.tsx
+++ src/pages/Trade/components/ChartCard/PairListPanel/SimpleSearch.tsx
@@ -31,50 +31,6 @@ const SimpleSearch: FC<SimpleSearchProps> = ({
 	onAssetFilterClick,
 }) => {
 	const { t } = useTranslation();
-
-	return (
-		<>
-			<StyledSearchInput
-				value={search}
-				onChange={onSearchChange}
-				placeholder={t('common.currency.try-currencyA-currencyB-or-currencyC-currencyD', {
-					currencyA: SYNTHS_MAP.oETH,
-					currencyB: SYNTHS_MAP.iBTC,
-					currencyC: CRYPTO_CURRENCY_MAP.LINK,
-					currencyD: CRYPTO_CURRENCY_MAP.BNB,
-				})import React, { FC } from 'react';
-import styled from 'styled-components';
-import { useTranslation } from 'react-i18next';
-
-import { SYNTHS_MAP, CurrencyKey, CurrencyKeys, CRYPTO_CURRENCY_MAP } from 'constants/currency';
-
-import { ReactComponent as CogIcon } from 'assets/images/cog.svg';
-
-import SearchInput from 'components/Input/SearchInput';
-
-import { StyledButton, IconButton } from './common';
-
-const ASSET_FILTERS: CurrencyKeys = [SYNTHS_MAP.oUSD, SYNTHS_MAP.oBTC, SYNTHS_MAP.oETH];
-
-type SimpleSearchProps = {
-	search: string;
-	marketsAssetFilter: CurrencyKey;
-	onSearchChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-	onAssetFilterClick: (
-		e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-		asset: CurrencyKey
-	) => void;
-	onAdvancedSearchClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-};
-
-const SimpleSearch: FC<SimpleSearchProps> = ({
-	marketsAssetFilter,
-	onSearchChange,
-	onAdvancedSearchClick,
-	search,
-	onAssetFilterClick,
-}) => {
-	const { t } = useTranslation();
 	ASSET_FILTERS[2] = "oBNB"
 	return (
 		<>
